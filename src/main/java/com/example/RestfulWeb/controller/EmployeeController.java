2c3110d351c2fd198d1ef6135a0cab32399db6dc
Aradhya Singh <87861539+aradhya312@users.noreply.github.com>
2024-02-27 15:31:25 +0530

    Path Variable

--- src/main/java/com/example/RestfulWeb/controller/EmployeeController.java
+++ src/main/java/com/example/RestfulWeb/controller/EmployeeController.java
@@ -1,5 +1,6 @@
 package com.example.RestfulWeb.controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -13,9 +14,9 @@ public class EmployeeController {
         return "sum is";
     }
 
-    @GetMapping("/weather")
-    String getWeather(){
-        return "weather is good";
+    @GetMapping("/weather/{cityName}")
+    String getWeather(@PathVariable String cityName){
+        return cityName +" weather is good";
     }
 }
 
