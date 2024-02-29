55fc672588765fa42de8b3267ea28c69a4161694
aditikiran <91718030+aditikiran@users.noreply.github.com>
2024-02-29 10:36:41 +0530

    aditi's branch

--- src/main/java/com/example/RestfulWeb/RestfulWebApplication.java
+++ src/main/java/com/example/RestfulWeb/RestfulWebApplication.java
@@ -9,6 +9,10 @@ public class RestfulWebApplication {
 	public static void main(String[] args) {
 		System.out.println("hello");
 		SpringApplication.run(RestfulWebApplication.class, args);
+
+		System.out.println( "This is Aditi's branch");
 	}
+
+
 
 }
